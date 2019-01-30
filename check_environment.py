ad0f510b6a13be691a3b1d55c37b979a20dcd4f3
ChristinaLast <36204574+ChristinaLast@users.noreply.github.com>
2019-01-30 10:18:03 +0000

    Update check_environment.py

--- check_environment.py
+++ check_environment.py
@@ -18,5 +18,5 @@ else:
             block_groups = geopandas.read_file("zip://./data/2010_block_groups.zip")
             print("All good.")
         except Exception as e:
-            print("Couldn't read blocks shapefile.")
+            print("Couldn't read block groups shapefile.")
             print(e)
